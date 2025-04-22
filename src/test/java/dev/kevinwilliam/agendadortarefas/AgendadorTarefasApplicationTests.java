cf9326e89a7542a881eec616f486413c851b40cf
Kevin Fernandes <kevinwinamp@gmail.com>
2025-04-21 21:18:37 -0300

    Commit inicial

---
+++ src/test/java/dev/kevinwilliam/agendadortarefas/AgendadorTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kevinwilliam.agendadortarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendadorTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
